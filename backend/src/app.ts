cab0365197d59ad8bc9dea94535a57986cac5e05
RitickLath <ritick943lath@gmail.com>
2025-04-18 20:10:53 +0530

    correction of app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,18 +18,18 @@ app.use(express.json());
 // Authentication API
 app.use("/api/v1/auth", authRouter);
 // User and Profile API
-app.use("/api/v1/auth", userRouter);
+app.use("/api/v1/user", userRouter);
 // Tweet API
-app.use("/api/v1/auth", tweetRouter);
+app.use("/api/v1/tweet", tweetRouter);
 // Comment API
-app.use("/api/v1/auth", commentRouter);
+app.use("/api/v1/comment", commentRouter);
 // Like API
-app.use("/api/v1/auth", likeRouter);
+app.use("/api/v1/likes", likeRouter);
 // Feed and TimeLine APi
-app.use("/api/v1/auth", feedRouter);
+app.use("/api/v1/feed", feedRouter);
 // Search API
-app.use("/api/v1/auth", searchRouter);
+app.use("/api/v1/search", searchRouter);
 // Notification API
-app.use("/api/v1/auth", notificationRouter);
+app.use("/api/v1/notification", notificationRouter);
 // Media API
-app.use("/api/v1/auth", mediaRouter);
+app.use("/api/v1/media", mediaRouter);
