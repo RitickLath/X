1d8ef69d21cf89b3bd45c1c1e050dd56574519a4
RitickLath <ritick943lath@gmail.com>
2025-05-01 03:17:44 +0530

    Redirection to new tab for Neonkit

--- frontend/src/component/CTAA.tsx
+++ frontend/src/component/CTAA.tsx
@@ -3,12 +3,24 @@ import { LandingStyles } from "../constants/style";
 import Button from "../Generic Components/Button";
 
 const CTAA: FC = () => {
+  const handleClick = () => {
+    // will add the api to let the analytics of A/B testing work
+  };
   return (
-    <Button
-      content="Explore Tailwind UI Kit"
-      className={LandingStyles.variantAButton}
-    />
+    <a
+      href="https://neonkit.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer" // 
+    >
+      <Button
+        content="Explore Tailwind UI Kit"
+        onClick={handleClick}
+        className={LandingStyles.variantAButton}
+      />
+    </a>
   );
 };
 
 export default CTAA;
+
+// learning: https://stackoverflow.com/questions/57628890/why-people-use-rel-noopener-noreferrer-instead-of-just-rel-noreferrer
--- frontend/src/component/CTAB.tsx
+++ frontend/src/component/CTAB.tsx
@@ -3,14 +3,24 @@ import { LandingStyles } from "../constants/style";
 import Button from "../Generic Components/Button";
 
 const CTAB: FC = () => {
+  const handleClick = () => {
+    // will add the api to let the analytics of A/B testing work
+  };
   return (
     <div className={LandingStyles.variantBWrapper}>
       <div className={LandingStyles.variantBInner}>
         <span className="font-semibold">Try our Tailwind Component Kit →</span>
-        <Button
-          content="Check it out"
-          className={LandingStyles.variantBButton}
-        />
+        <a
+          href="https://neonkit.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button
+            content="Check it out"
+            onClick={handleClick}
+            className={LandingStyles.variantBButton}
+          />
+        </a>
       </div>
     </div>
   );
